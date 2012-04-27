f25102c16600e7fbd2a10473cfc454f5f0ec12d6
Zete Lui <ns@theplant.jp>
2012-04-27 11:56:51 +0800

    remove comments that repeat code

--- lib/simba/base_app/test/test_helper.rb
+++ lib/simba/base_app/test/test_helper.rb
@@ -1,15 +1,14 @@
 ENV['RACK_ENV'] ||= "test"
 require_relative "../config/boot.rb"
+
 # factory_girl
 dir = File.expand_path File.dirname(__FILE__)
 Dir.glob "#{dir}/factory/**/*_factory.rb" do |f|
   require f
 end
-# minitest
+
 require "minitest/autorun"
-# rack test
 require "rack/test"
-# database_clean
 DatabaseCleaner.strategy = :truncation
 
 class TestCase < MiniTest::Unit::TestCase
